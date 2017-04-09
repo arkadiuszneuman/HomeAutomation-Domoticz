3325d24ba6ffd679bd1fba757f50e85de6f8f4a3
Arkadiusz Neuman <arkadiusz.neuman@btskyrise.com>
2017-04-09 20:54:16 +0200

    Fixed weather icon.

--- src/app/main/components/weather/weather.component.ts
+++ src/app/main/components/weather/weather.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OpenWeatherMapApiService } from './service/open-weather-map-api.service'
 import { WeathersIconsApi } from './weather-icons-api'
-
+import { WeatherNameToIconPipe } from 'ng2-weather-icons';
 
 @Component({
   selector: 'weather',
@@ -16,7 +16,8 @@ export class WeatherComponent implements OnInit {
   public pressure: number;
   public humidity: number;
 
-  constructor(private apiService: OpenWeatherMapApiService) { }
+  constructor(private apiService: OpenWeatherMapApiService,
+    private weatherNameToIconPipe: WeatherNameToIconPipe) { }
 
   ngOnInit() {
     this.refreshWeather();
@@ -31,7 +32,8 @@ export class WeatherComponent implements OnInit {
       .subscribe(weather => {
 
         console.log(weather);
-        this.weatherIcon = this.weatherIcons.icons[weather.weather[0].id].icon;
+        
+        this.weatherIcon = 'wi ' + this.weatherNameToIconPipe.transform(weather.weather[0].id);
         this.temp = Math.round(weather.main.temp * 10) / 10;
         this.pressure = weather.main.pressure;
         this.humidity = weather.main.humidity;
